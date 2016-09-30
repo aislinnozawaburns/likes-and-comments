ba75d6a3d0340a513cfa84f9b455196605633478
aislinnozawaburns <aislinnozawaburns@Aislinns-MacBook-Pro.local>
2016-09-30 12:16:23 -0700

    asdf

--- script.js
+++ script.js
@@ -26,23 +26,28 @@ var comments = document.querySelector("#comments");
 
 // var button = document.querySelector("button");
 
-newComment.addEventListener("click", createPost);
+newComment.addEventListener("submit", createPost);
 
 
 function createPost(event) {
   event.preventDefault();
 
 var comment = document.createElement("div");
-
-var parent = document.querySelector("#comments");
-var form = document.createElement("form");
-var textarea = document.querySelector("textarea").value;
-var p = document.createElement("p");
-valueOfTextArea = textarea.value;
-parent.appendChild(form);
-parent.appendChild(p);
-p.textContent = textarea;
-
-
+comment.classList.add("comment");
+comment.textContent = commentBody.value;
+comments.appendChild (comment);
 newComment.reset();
 }
+//
+// var parent = document.querySelector("#comments");
+// var form = document.createElement("form");
+// var textarea = document.querySelector("textarea").value;
+// var p = document.createElement("p");
+// valueOfTextArea = textarea.value;
+// parent.appendChild(form);
+// parent.appendChild(p);
+// p.textContent = textarea;
+//
+//
+// newComment.reset();
+// }
